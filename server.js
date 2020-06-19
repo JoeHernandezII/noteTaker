a45c638b4385af3ac12ed3991289656317d1ceef
Joe Hernandez <yoshi.jhii@gmail.com>
2020-06-18 21:58:24 -0500

    updates to server.js

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
+//Dependencies needed
 const express = require("express");
 const apiRoutes = require("./routes/apiRoutes");
 const htmlRoutes = require("./routes/htmlRoutes");
 
-// Initialize the app and create a port
+// Creating exress server and port
 const app = express();
 const PORT = process.env.PORT || 1313;
 
-// Set up body parsing, static, and route middleware
+// The data parsing with express app
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
 
-// Start the server on the port
+// Listening on the port
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
